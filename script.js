c67123f01a32ce79e50c651b9f7630cb53753fd1
Rachel Reilly <r.a.reill18@gmail.com>
2020-08-11 20:48:57 -0500

    added content object for STORE[1-5] and named functions and made comments on their intended actions

--- script.js
+++ script.js
@@ -4,18 +4,25 @@
 
 
 $('button').on('click', renderQuiz);
+//eventually want .on(renderQuiz) to call 
+//createButton, createContent
+
 function renderQuiz(event) {
+    //i=0??
     for (let i = 0; i < STORE.length; i++) {
         $('button').html(STORE[i].button);
     }
 
-}
-
-function pages() {
+    //this is not really cycling through each of the items. 
 
 }
+//cycle through the array on click and call every one of these functions
+
+
 //give STORE[i].button html formatting to become a button 
 function createButton() {
+    let buttonReady = `<button type="button>${STORE[i].button}</button>`;
+    return buttonReady;
 
 }
 
@@ -24,17 +31,29 @@ function createButton() {
 
 //give STORE[i].content html formatting to become valid content
 function createContent() {
+    //logic to see if STORE[x].content needs building
+    //if true call:
+    //can use switch based on content as string or obj
+     //buildQuiz();
 
 }
 
-//is it bad to store heading code in the object?
+function buildQuiz(){
+//call this function with STORE[1-5] to build 
+//calls buildQuizQuestion
+}
 
 
-//make separate func for cycling through array
-//we want to move headings outside main without altering the layout
-//body flex to column
-//or make div with class exclusive to inner content, not including button
-//object for each of the pages with format changes to html. 
+function buildQuizQuestions(){
+    
+    //forEach STORE[x].content
+    //access element in content array
+    return `<div class="answer">
+    <input type="radio" name="${STORE[X].name}" value="${STORE[X].content[X].value}">
+    <label for="WHAT SHOULD I PUT HERE">${STORE[X].content[X].answer}</label><br>
+</div>`
+}
+
 
 
 
@@ -51,16 +70,15 @@ function createContent() {
 //because this will help scale content in quiz 
 
 //create variable for SCORE at $(.currentScore)
+//figure out how to track and use this data
 
-//make functions that add <button> 
+//make functions that add <button> and formats content
 
 //set state of button with ! to be able to press again
 
+//change formatted answers to objects run through a function
 
-
-
-
-
+//research feilds for RADIO BUTTONS!!!
 
 
 
@@ -71,7 +89,6 @@ function createContent() {
 
 
 function main() {
-//call all other functions here
 }
 
 $(main);
--- store.js
+++ store.js
@@ -4,40 +4,145 @@ const STORE = [
   {
     index: 0, 
     button:'Start Quiz!', 
-    heading:'', 
+    heading:'<h1></h1>', 
     content:''
 },
 
   {
     index: 1, 
-    button:'Next', 
-    heading:'', 
-    content:''
+    button:'Submit', 
+    heading:'<h2>What does HTML stand for?</h2>', 
+    content:[
+      {
+        index:0, 
+        answer:'',
+        value: false
+      },
+      {
+        index:1, 
+        answer:'',
+        value:false 
+      },
+      {
+        index:2, 
+        answer:'',
+        value: false
+      },
+      {
+        index:3, 
+        answer:'',
+        value: true
+      }
+    ]
 },
 
   {
     index: 2, 
     button:'Next', 
-    heading:'', 
-    content:''
+    heading:'<h2>What is an example of Proceedural programming?</h2>', 
+    content: [
+      {
+        index:0, 
+        answer:'',
+        value: false
+      },
+      {
+        index:1, 
+        answer:'',
+        value:true 
+      },
+      {
+        index:2, 
+        answer:'',
+        value: false
+      },
+      {
+        index:3, 
+        answer:'',
+        value: false
+      }
+    ]
 },
   {
     index: 3, 
     button:'Next', 
-    heading:'', 
-    content:''
+    heading:'<h2>What is jQuery?</h2>', 
+    content:[
+      {
+        index:0, 
+        answer:'A JavaScript Framework',
+        value: false
+      },
+      {
+        index:1, 
+        answer:'XXX',
+        value: false
+      },
+      {
+        index:2, 
+        answer:'XXX',
+        value: false
+      },
+      {
+        index:3, 
+        answer:'A Javascript Library',
+        value: true
+      }
+    ]
   },
   {
     index: 4, 
     button:'Next', 
-    heading:'', 
-    content:''
+    heading:'<h2>What is OOP?</h2>', 
+    content: [
+      {
+        index:0, 
+        answer:'',
+        value: true
+      },
+      {
+        index:1, 
+        answer:'',
+        value: false 
+      },
+      {
+        index:2, 
+        answer:'',
+        value: false
+      },
+      {
+        index:3, 
+        answer:'',
+        value: false
+      }
+    ]
   },
   {
     index: 5, 
     button:'Next', 
-    heading:'', 
-    content:''
+    heading:'<h2>What is the primary programming language used for web development?</h2>', 
+    content:[
+      {
+        index:0, 
+        answer:'',
+        value: false
+      },
+      {
+        index:1, 
+        answer:'',
+        value:true 
+      },
+      {
+        index:2, 
+        answer:'',
+        value: false
+      },
+      {
+        index:3, 
+        answer:'',
+        value: false
+      }
+    ]
   },
   {
     index: 6, 
